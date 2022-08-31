b810a0a3297209b792009a44380e3f0f1681b396
Gabriel <gopxfs@hotmail.com>
2022-08-31 19:39:34 -0300

    Add case to test #new

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,4 +4,10 @@ describe Solver do
   before :each do
     @solver = Solver.new
   end
+
+  describe "#new" do
+    it "return a Solver object" do
+      expect(@solver).to be_an_instance_of Solver
+    end
+  end
 end
