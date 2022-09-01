42bedd516474c6557d3be6635ea3d48b70ab8ca0
Gabriel <gopxfs@hotmail.com>
2022-08-31 21:02:55 -0300

    Add custom error messages

--- solver.rb
+++ solver.rb
@@ -1,5 +1,11 @@
+require_relative './errors'
+
 class Solver
   def factorial(number)
+    unless number.class == Integer && number >= 0
+      raise InvalidArgumentError, "Argument should be a positive integer."
+    end
+
     result = 1
     (1..number).each do |n|
       result *= n
